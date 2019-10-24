1712939f4f2aebd73a7c95e67f52f7607a25d3c2
Jack Kelly <jack.kelly@data61.csiro.au>
2019-10-24 15:07:49 +1000

    Push semialign-diff into subdir

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
-(("src" . ((nil . ((dante-target . "semialign-diff")))))
- ("doctests" . ((nil . ((dante-target . "doctests")))))
+(("semialign-diff/src" . ((nil . ((dante-target . "semialign-diff")))))
+ ("semialign-diff/doctests" . ((nil . ((dante-target . "doctests")))))
+ ("semialign-merge/src" . ((nil . ((dante-target . "semialign-merge")))))
  (nil . ((dante-repl-command-line
           . ("nix-shell" "--run"
 (concat "cabal new-repl " dante-target " --builddir=dist/dante"))))))
