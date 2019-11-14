72928fa866340dffbcbb000627b359454527ee6a
Jack Kelly <jack.kelly@data61.csiro.au>
2019-11-14 14:07:08 +1000

    Remove backup file, document Data.Semialign.Merge

--- src/Data/Semialign/Merge.hs
+++ src/Data/Semialign/Merge.hs
@@ -1,3 +1,43 @@
+{-|
+
+Module      : Data.Semialign.Merge
+Copyright   : (c) 2019, Commonwealth Scientific and Industrial Research Organisation
+License     : BSD3
+Maintainer  : jack.kelly@data61.csiro.au
+Stability   : experimental
+Portability : Portable
+
+The 'Semialign' typeclass lets us line up two structures of the same
+type. We can use this to merge two structures, or add additional
+typeclasses to do filtering, 'Applicative' effects, or tracking
+indices.
+
+'merge' is the simplest function. It takes five arguments: functions
+to handle values present in the left/right/both structures and two
+structures to merge.
+
+@
+merge :: Semialign t
+  => (a -> c) -> (b -> c) -> (a -> b -> c)
+  -> t a -> t b
+  -> t c
+@
+
+Every other function in this module is a variant with functionality
+based on its name:
+
+* Prefix @i@ means an "indexed" variant: each function argument takes
+  an additional index (@i@) parameter.
+
+* Suffix @Maybe@ means "filter results": each function argument
+  returns @Maybe c@, and @Nothing@s are filtered out.
+
+* Suffix @A@ means "applicative": each function argument returns
+  actions in some 'Applicative' @f@, and the whole operation collects
+  these actions to produce the a merged structure in @f@.
+
+-}
+
 module Data.Semialign.Merge
   ( merge
   , mergeMaybe
@@ -16,6 +56,7 @@ import Data.Semialign.Indexed (SemialignWithIndex(..))
 import Data.These (these)
 import Data.Witherable (Filterable(..), Witherable(..))
 
+-- | @since 0.1.0.0
 merge
   :: Semialign t
   => (a -> c)
@@ -26,6 +67,7 @@ merge
   -> t c
 merge f g h = alignWith (these f g h)
 
+-- | @since 0.1.0.0
 mergeMaybe
   :: (Filterable t, Semialign t)
   => (a -> Maybe c)
@@ -36,6 +78,7 @@ mergeMaybe
   -> t c
 mergeMaybe f g h = (catMaybes .) . alignWith (these f g h)
 
+-- | @since 0.1.0.0
 mergeA
   :: (Applicative f, Semialign t, Traversable t)
   => (a -> f c)
@@ -46,6 +89,7 @@ mergeA
   -> f (t c)
 mergeA f g h = (sequenceA .) . alignWith (these f g h)
 
+-- | @since 0.1.0.0
 mergeMaybeA
   :: (Applicative f, Semialign t, Witherable t)
   => (a -> f (Maybe c))
@@ -56,6 +100,7 @@ mergeMaybeA
   -> f (t c)
 mergeMaybeA f g h = (wither id .) . alignWith (these f g h)
 
+-- | @since 0.1.0.0
 imerge
   :: (SemialignWithIndex i t)
   => (i -> a -> c)
@@ -66,6 +111,7 @@ imerge
   -> t c
 imerge f g h = ialignWith (these <$> f <*> g <*> h)
 
+-- | @since 0.1.0.0
 imergeMaybe
   :: (Filterable t, SemialignWithIndex i t)
   => (i -> a -> Maybe c)
@@ -76,6 +122,7 @@ imergeMaybe
   -> t c
 imergeMaybe f g h = (catMaybes .) . ialignWith (these <$> f <*> g <*> h)
 
+-- | @since 0.1.0.0
 imergeA
   :: (Applicative f, SemialignWithIndex i t, Traversable t)
   => (i -> a -> f c)
@@ -86,6 +133,7 @@ imergeA
   -> f (t c)
 imergeA f g h = (sequenceA .) . ialignWith (these <$> f <*> g <*> h)
 
+-- | @since 0.1.0.0
 imergeMaybeA
   :: (Applicative f, SemialignWithIndex i t, Witherable t)
   => (i -> a -> f (Maybe c))
